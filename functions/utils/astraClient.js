9343a695596bbfba0df18c97483aad29902fd66b
akashsangami <akashsangami@gmail.com>
2021-07-19 16:11:53 +0000

    lala

--- functions/utils/astraClient.js
+++ functions/utils/astraClient.js
@@ -6,9 +6,9 @@ const getAstraClient = async () => {
   if (astraClient === null) {
     astraClient = await createClient(
       {
-        astraDatabaseId: process.env.ASTRA_DB_ID,
-        astraDatabaseRegion: process.env.ASTRA_DB_REGION,
-        applicationToken: process.env.ASTRA_DB_APPLICATION_TOKEN,
+        astraDatabaseId: process.env.NlZzPpFZOAzNOhQZZhQPMvIa,
+        astraDatabaseRegion: process.env.asia-south1,
+        applicationToken: process.env.AstraCS:NlZzPpFZOAzNOhQZZhQPMvIa:1cfc22a2d8a2c023e1d1464a93cd34bc522d2e662bc3120a2fe5cfbe31ae3a09,
       },
       30000
     );
